99344d367d6e976978f6ed16f34cbc81790089e0
yeyevtushenko <yehor803@gmail.com>
2023-12-18 19:29:55 +0200

    конвертування одиниць виміру

--- Task 3.py
+++ Task 3.py
@@ -5,4 +5,23 @@ class LengthConverter:
     METERS_TO_FEET = 3.28084
     CENTIMETERS_TO_INCHES = 0.393701
 
+    @staticmethod
+    def convert_meters_to_feet(meters):
+        """
+        Convert length from meters to feet.
+        """
+        return meters * LengthConverter.METERS_TO_FEET
+
+    @staticmethod
+    def convert_feet_to_meters(feet):
+        return feet / LengthConverter.METERS_TO_FEET
+
+    @staticmethod
+    def convert_centimeters_to_inches(centimeters):
+        return centimeters * LengthConverter.CENTIMETERS_TO_INCHES
+
+    @staticmethod
+    def convert_inches_to_centimeters(inches):
+        return inches / LengthConverter.CENTIMETERS_TO_INCHES
+
 
